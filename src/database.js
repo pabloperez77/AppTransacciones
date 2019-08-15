0cd38bfed5f74df33eb7640ea16e5e3fd30fb6a1
Pablo <pablopereznovoa@gmail.com>
2019-08-15 10:26:32 +0200

    conexión a BD y establecimiento variable de entorno

---
+++ src/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify');
+
+console.log(process.env.MONGODB_URI);
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err));
---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const app = require('./app');
+require('./database');
+
+async function main(){
+    await app.listen(4000);
+    console.log('Server on PORT 4000');
+}
+
+main();
